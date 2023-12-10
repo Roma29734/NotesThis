b3e889accc3c6b8c88cfaaab624a21bf4017ba11
roma29733@gmail.com <roma29734@gmail.com>
2023-12-10 19:32:33 +0300

    add detailRemoteNote

--- src/screen/detailNotes/DetailRemoteNote.tsx
+++ src/screen/detailNotes/DetailRemoteNote.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { Alert, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { ActivityIndicator, Alert, Text, TextInput, TouchableOpacity, View } from "react-native";
 import Style from "../../viewComponents/Main.basic.style";
 import HeaderBarTitleButton from "../../viewComponents/HeaderBarTitleButton";
 import { useThemeColor } from "../../assets/Theme";
+import axios from "axios";
+import { Application_Id, REST_API_Key } from "../../../Keys";
 
 export const DetailRemoteNote = ({ route, navigation }: any) => {
 
@@ -11,7 +13,7 @@ export const DetailRemoteNote = ({ route, navigation }: any) => {
   const [textInputTitle, onChangeTextInputTitle] = useState(transmittedTodoItemRemote.title);
   const [textInputSubTitle, onChangeTextInputSubTitle] = useState(transmittedTodoItemRemote.subTitle);
   const [currentDataTime, setCurrentDataTime] = useState("");
-
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     var date = new Date().getDate()
@@ -22,6 +24,31 @@ export const DetailRemoteNote = ({ route, navigation }: any) => {
   }, []);
 
 
+  const updateNoteList = () => {
+    setIsLoading(true);
+    let data = JSON.stringify({
+      "title": textInputTitle,
+      "subTitle": textInputSubTitle,
+      "createData": currentDataTime
+    });
+
+    axios.put(`https://parseapi.back4app.com/classes/noteList/${transmittedTodoItemRemote.objectId}`, `${data}`, {
+      headers: {
+        "X-Parse-Application-Id": Application_Id,
+        "X-Parse-REST-API-Key": REST_API_Key,
+        "Content-Type": "application/json"
+      }
+    }) .then(({ data }) => {
+      console.log(data);
+      Alert.alert("Complete", "Update note to complete");
+    }).catch(err => {
+      console.log(err);
+    }).finally(() => {
+        setIsLoading(false);
+    }
+    );
+  };
+
   const BackHandler = () => {
     navigation.pop();
   };
@@ -30,6 +57,17 @@ export const DetailRemoteNote = ({ route, navigation }: any) => {
     // deleteItem(transmittedTodoItem.id);
   };
 
+  const checkInputData = (): boolean => {
+    if (!textInputTitle.trim()) {
+      return false;
+    } else {
+      if (!textInputSubTitle.trim()) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+  };
 
   const colorTheme = useThemeColor();
   const styleComponent = Style(colorTheme);
@@ -39,6 +77,9 @@ export const DetailRemoteNote = ({ route, navigation }: any) => {
 
       <HeaderBarTitleButton title={"Yours Note"} BackHandler={BackHandler} showDeleteItem={true}
                             DeleteHandler={DeleteHandler} />
+
+
+      {isLoading && <ActivityIndicator size={"large"} style={{position: 'absolute', top: 0, left: 0, right: 0, bottom: 0,}} />}
 
       <TextInput style={styleComponent.inputTitle}
                  placeholder="Enter your title notes"
@@ -54,11 +95,11 @@ export const DetailRemoteNote = ({ route, navigation }: any) => {
                  value={textInputSubTitle} />
 
       <TouchableOpacity style={styleComponent.buttonAdd} onPress={() => {
-        // if (checkInputData() == true) {
-        //   updateTodo();
-        // } else {
-        //   Alert.alert("Unable to update a note", "Fill in all the fields to update a note");
-        // }
+        if (checkInputData() == true) {
+          updateNoteList();
+        } else {
+          Alert.alert("Unable to update a note", "Fill in all the fields to update a note");
+        }
       }}>
         <Text style={styleComponent.textFromButtonAdd}>Update</Text>
       </TouchableOpacity>
